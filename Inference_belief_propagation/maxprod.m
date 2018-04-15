73fcdbae514b81a572b2c28d5a79cdc5f56dab4e
Hao Xiong <haoxiong@outlook.com>
2018-04-15 03:27:44 -0500

    Update maxprod.m
    correct the max-product (max-sum) algorithm

--- Inference_belief_propagation/maxprod.m
+++ Inference_belief_propagation/maxprod.m
@@ -1,19 +1,12 @@
-function X = maxprod(A, w, its)
-%function [Z, blfX, blfC,MsgC2V,MsgV2C] = sumprod(A, w, its)
-%The function should return the approximate partition function attained after its
-%iterations by plugging the appropriately normalized beliefs into the Bethe free energy.
-% A: adjacency matrix of the graph G; w: vector representing the weights, its: the number of iterations
+function Assignment = maxprod(A, w, its)
 N = size(A, 1);
 K = length(w);
 if ~issymmetric(A)
     error('Adjacency matrix A must be symmetric!');
 end
-
-%scaling factors
-eta1 = 0.1;
-eta2 = 0.1;
-MsgC2V = ones(N,N,K);%Initialize Cliuqe to variable message table:row->clique;col->var;page->color
-MsgV2C = ones(N,N,K);%Initialize Variable to clique message table:row->var;col->clique;page->color
+MsgC2V = zeros(N,N,K);%Initialize Cliuqe to variable message table:row->clique;col->var;page->color
+MsgV2C = zeros(N,N,K);%Initialize Variable to clique message table:row->var;col->clique;page->color
+backtracing = cell(N,N,K);
 for t = 1:its
     % Update the message passing from clique c to variable v
     for c = 1:N
@@ -22,17 +15,26 @@ for t = 1:its
         if degree > 1 %This is an valid clique marked by c; Filter out tree-like leaves
             for dg = 1:degree
                 v = nodes(dg);
-                Msg = squeeze(MsgV2C(nodes(nodes~=v),c,:));
+                neibrs = nodes(nodes~=v);
+                Msg = squeeze(MsgV2C(neibrs,c,:));
                 width = degree - 1;
                 for xi = 1:K
                     Xc = permn([1:xi-1 xi+1:K],width);% get all the possible value combination
+                    max_msg = 0;
                     for r = 1:size(Xc,1)
-                        if width == numel(unique(Xc(r,:)))
-                            MsgC2V(c,v,xi) = MsgC2V(c,v,xi) + prod(Msg(sub2ind([width K],1:width,Xc(r,:))));
+                        xc = Xc(r,:);
+                        if width == numel(unique(xc))
+                            sum_msg = sum(Msg(sub2ind([width K],1:width,xc)));
+                            if(sum_msg > max_msg)
+                                max_msg = sum_msg;
+                                if t==its %back-tracing storage
+                                    backtracing{c,v,xi} = xc;
+                                end
+                            end
                         end
                     end
+                    MsgC2V(c,v,xi) = max_msg /2;
                 end
-                MsgC2V(c,v,:) = MsgC2V(c,v,:) * eta1;
             end
         end
     end
@@ -40,70 +42,42 @@ for t = 1:its
     for v = 1:N
         for c = 1:N
             if (A(v,c) > 0)%This is a valid variable marked by v->c; Filter out non-edges
-                MsgV2C(v,c,:) = exp(w) .* squeeze(MsgC2V(v,c,:))' * eta2;
+                MsgV2C(v,c,:) = (w + squeeze(MsgC2V(v,c,:))') /2;
             end
         end
     end
     
 end
 
-%store the beliefs of variables (a.k.a the color of each edge) key=>edge'i-j'; color domain=> 1:k
-blfX = zeros(N,N,K);
+Assignment = zeros(N,N);
 for i = 1:N
     for j = i+1:N
         if (A(i,j)>0)
-            blfx = exp(w) .* squeeze(MsgC2V(i,j,:) .* MsgC2V(j,i,:))';
-            sm = sum(blfx);
-            if sm ~= 0
-                blfX(i,j,:) = blfx / sm;% normalize variable beliefs
+            maxmaginal = w + squeeze(MsgC2V(i,j,:) + MsgC2V(j,i,:))';
+            [~,m_color] = max(maxmaginal);
+            if Assignment(i,j) == 0
+                Assignment(i,j) = m_color;
+                %then we could get the configuration of clique i and clique j based on the m_color and back-tracing storage
+                inodes = find(A(i,:));
+                ineibrs = inodes(inodes~=j);
+                if all(Assignment(i,ineibrs)==0)
+                    Assignment(i,ineibrs) = backtracing{i,j,m_color};
+                    Assignment(ineibrs,i) = backtracing{i,j,m_color};
+                end
+            end
+            if Assignment(j,i) == 0
+                Assignment(j,i) = m_color;
+                jnodes = find(A(j,:));
+                jneibrs = jnodes(jnodes~=i);
+                if all(Assignment(j,jneibrs)==0)
+                    Assignment(j,jneibrs) = backtracing{j,i,m_color};
+                    Assignment(jneibrs,j) = backtracing{j,i,m_color};
+                end
             end
         end
     end
 end
 
-%store the beliefs of cliques (a.k.a the assignments of color for the incidented edges of node i);
-blfC = cell(1,N);
-for c = 1:N
-    V = find(A(c,:));
-    num = numel(V);
-    if num > 1
-        blfc = zeros(ones(1,num)*K);
-        Msg = squeeze(MsgV2C(V,c,:));
-        XC = permn(1:K,num);% get all the possible value combination
-        for r = 1:size(XC,1)
-            Xr = XC(r,:);
-            if num == numel(unique(Xr))
-                temp = num2cell(Xr);
-                ind1 = sub2ind(size(blfc),temp{:});
-                ind2 = sub2ind([num K],1:num,Xr);
-                blfc(ind1) = prod(Msg(ind2));
-            end
-        end
-        sm = sum(blfc(:));
-        if(sm ~= 0)
-            blfc = blfc / sm;
-        end
-        blfC{c} = blfc;
-    end
-end
-
-X = A;
-
-for i =1:N
-    nodes = find(A(i,:));
-    num = numel(nodes);
-    if num > 1
-        blf_C = blfC{i};
-        [~,position] = max(blf_C(:));
-        clen = length(size(blf_C));
-        indices = cell(1,clen);
-        [indices{:}] = ind2sub(size(blf_C),position);
-        for n=1:clen
-            X(i,nodes(n)) = indices{n};
-            X(nodes(n),i) = indices{n};
-        end
-    end
-end
 end
 
 function M = permn(V, N)
